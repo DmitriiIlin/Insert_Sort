70ee353eb305a7044308e90aa1f77363ef271019
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2019-08-19 07:27:30 +0300

    Добавлена реализация сортировки Шелла с шагом Кнута

--- Insert_Sort.py
+++ Insert_Sort.py
@@ -6,6 +6,32 @@ def Conditions(array,step,start):
         return False
 
 
+
+def KnuthSequence(array_size):
+    #Ф-ция вычесляет интервальную последовательность целых чисел
+    Numbers=[]
+    Knuth_Numbers=[]
+    number=1
+    if array_size>=1:
+        while array_size>number:
+            Numbers.append(number)
+            number=3*number+1
+        Numbers_size=len(Numbers)-1
+        while Numbers_size>=0:
+            Knuth_Numbers.append(Numbers[Numbers_size])
+            Numbers_size-=1
+        return Knuth_Numbers
+    else:
+        return []
+
+
+def ShellSort(array):
+    #Сортировка массива методом Шелла
+    Knuth_numbers=KnuthSequence(len(array))
+    for i in range(0,len(Knuth_numbers)):
+        array=InsertionSortStep(array,Knuth_numbers[i],0)
+    return array        
+
 def InsertionSortStep(array,step=1,start=0):
     # Сортировка вставкой в массиве array, с шагом step, номер элемента i
     test=Conditions(array,step,start)
@@ -16,7 +42,6 @@ def InsertionSortStep(array,step=1,start=0):
             if i<len(array) and i>=0:
                 while array[i]>key and i>=0:
                     array[i+step]=array[i]
-
                     i-=step 
                 else: 
                     array[i+step]=key
@@ -28,7 +53,9 @@ def InsertionSortStep(array,step=1,start=0):
 
 
 """
-a=[1,6,5,4,3,2,7]
+a=[1,6,5,4,3,2,7,67,44,23,567,12,45,67]
 print(a)
-print(InsertionSortStep(a,3,1))
+#print(InsertionSortStep(a,3,1))
+print(KnuthSequence(len(a)))
+print(ShellSort(a))
 """
